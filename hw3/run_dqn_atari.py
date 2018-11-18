66429de7d7d7f9ab2dbb72c2091b33e44e04ab45
Novin Shahroudi <novinsha@gmail.com>
2018-11-18 20:01:24 +0000

    Change the tmp filename log to vavoid conflict on hpc.

--- hw3/run_dqn_atari.py
+++ hw3/run_dqn_atari.py
@@ -75,7 +75,7 @@ def atari_learn(env,
         frame_history_len=4,
         target_update_freq=10000,
         grad_norm_clipping=10,
-        double_q=True
+        double_q=False
     )
     env.close()
 
@@ -109,7 +109,7 @@ def get_env(task, seed):
     set_global_seeds(seed)
     env.seed(seed)
 
-    expt_dir = '/tmp/hw3_vid_dir2/'
+    expt_dir = '/tmp/hw3_vid_dir2_novin/'
     env = wrappers.Monitor(env, osp.join(expt_dir, "gym"), force=True)
     env = wrap_deepmind(env)
 
